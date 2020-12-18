742cca5e5604ba364cb13127076000fe870e4b49
DESKTOP-NSJ3PD8\86152 <781407323@qq.com>
2020-12-18 09:52:06 +0800

    SF quota

--- user_model/src/main/java/com/springboot/mapper/StdLegalDataStructuredMapper.java
+++ user_model/src/main/java/com/springboot/mapper/StdLegalDataStructuredMapper.java
@@ -3,6 +3,8 @@ package com.springboot.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.springboot.domain.risk.StdLegalDataStructured;
 
-public interface StdLegalDataStructuredMapper extends BaseMapper<StdLegalDataStructured> {
+import java.util.Map;
 
+public interface StdLegalDataStructuredMapper extends BaseMapper<StdLegalDataStructured> {
+    public Map getvarname(Map map);
 }
---
+++ user_model/src/main/java/com/springboot/service/StdLegalService.java
@@ -0,0 +1,9 @@
+package com.springboot.service;
+
+public interface StdLegalService {
+    /**
+     * 生成司法中间表
+     * @param reqId
+     */
+    public void createStdLegalMidTable(String reqId);
+}
