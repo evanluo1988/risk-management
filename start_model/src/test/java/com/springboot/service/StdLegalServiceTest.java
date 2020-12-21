849f1388c0823805d242d2ccb85e65d649139d8a
DESKTOP-NSJ3PD8\86152 <781407323@qq.com>
2020-12-21 15:05:09 +0800

    Mock

--- start_model/src/test/java/com/springboot/service/StdLegalServiceTest.java
+++ start_model/src/test/java/com/springboot/service/StdLegalServiceTest.java
@@ -1,15 +1,34 @@
 package com.springboot.service;
 
 import com.springboot.ApplicationTest;
+import com.springboot.domain.risk.StdLegalDataStructured;
+import com.springboot.service.impl.StdLegalServiceImpl;
+import org.assertj.core.util.Lists;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 public class StdLegalServiceTest extends ApplicationTest {
-    @Autowired
-    private StdLegalService stdLegalService;
+    @InjectMocks
+    private StdLegalServiceImpl stdLegalService;
+
+    @Mock
+    private StdLegalDataStructuredService stdLegalDataStructuredService;
 
     @Test
     public void testCreateStdLegalMidTable() {
+        List<StdLegalDataStructured> stdLegalDataStructuredList = Lists.newArrayList();
+
+        StdLegalDataStructured s1 = new StdLegalDataStructured();
+        stdLegalDataStructuredList.add(s1);
+
+        Mockito.when(stdLegalDataStructuredService.findStdLegalDataStructuredByReqId("1b20d84f-3e71-41c6-8430-abd41af63016")).thenReturn(stdLegalDataStructuredList);
+
         stdLegalService.createStdLegalMidTable("1b20d84f-3e71-41c6-8430-abd41af63016");
+
     }
 }
