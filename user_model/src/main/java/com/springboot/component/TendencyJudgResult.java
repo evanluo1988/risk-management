a6ff9e557a13457d028e8cba4c965762ccb1cb76
DESKTOP-NSJ3PD8\86152 <781407323@qq.com>
2020-12-22 17:13:35 +0800

    SF quota UT

--- user_model/src/main/java/com/springboot/component/TendencyJudgResult.java
+++ user_model/src/main/java/com/springboot/component/TendencyJudgResult.java
@@ -5,6 +5,7 @@ import com.springboot.model.VerdictResultModel;
 import com.springboot.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -21,6 +22,9 @@ public class TendencyJudgResult implements QuotaComponent{
     @Override
     public String execQuota(String reqId) {
         List<VerdictResultModel> verdictResultModelList = stdLegalDataStructuredTempMapper.getVerdictResult(reqId);
+        if(CollectionUtils.isEmpty(verdictResultModelList)) {
+            return null;
+        }
         Map<String,List<VerdictResultModel>> verdictResultModelMap = Utils.getList(verdictResultModelList).stream().collect(Collectors.groupingBy(VerdictResultModel::getSentenceEffect));
         int count1 = Utils.getList(verdictResultModelMap.get("99")).size();
         int count2 = Utils.getList(verdictResultModelMap.get("0")).size();
