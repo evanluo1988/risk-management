e19612816e87265f1f63548e8678369e5e6eb678
DESKTOP-NSJ3PD8\86152 <781407323@qq.com>
2020-12-22 11:35:19 +0800

    SF quota 涉诉频率

---
+++ user_model/src/main/java/com/springboot/component/LitigaFrequency.java
@@ -0,0 +1,110 @@
+package com.springboot.component;
+
+import com.google.common.collect.Maps;
+import com.springboot.domain.risk.StdLegalDataStructuredTemp;
+import com.springboot.domain.risk.StdLegalEntUnexecutedTemp;
+import com.springboot.domain.risk.StdLegalEnterpriseExecutedTemp;
+import com.springboot.mapper.StdLegalDataStructuredTempMapper;
+import com.springboot.mapper.StdLegalEntUnexecutedTempMapper;
+import com.springboot.mapper.StdLegalEnterpriseExecutedTempMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 涉诉频率
+ */
+@Component("LitigaFrequency")
+public class LitigaFrequency implements QuotaComponent{
+
+    private static final String REG_PATTERN = "(（\\d{4}）)";
+
+    @Autowired
+    private StdLegalDataStructuredTempMapper stdLegalDataStructuredTempMapper;
+    @Autowired
+    private StdLegalEnterpriseExecutedTempMapper stdLegalEnterpriseExecutedTempMapper;
+    @Autowired
+    private StdLegalEntUnexecutedTempMapper stdLegalEntUnexecutedTempMapper;
+
+    @Override
+    public String execQuota(String reqId) {
+        Map<String, Object> paramMap = Maps.newHashMap();
+        paramMap.put("req_id", reqId);
+        List<StdLegalDataStructuredTemp> structuredList = stdLegalDataStructuredTempMapper.selectByMap(paramMap);
+        List<StdLegalEnterpriseExecutedTemp> enterpriseExecutedList = stdLegalEnterpriseExecutedTempMapper.selectByMap(paramMap);
+        List<StdLegalEntUnexecutedTemp> entUnexecutedList = stdLegalEntUnexecutedTempMapper.selectByMap(paramMap);
+
+        //<案件年份，案件数量>
+        Map<Integer, Integer> caseMap = Maps.newHashMap();
+        for(StdLegalDataStructuredTemp structured : structuredList) {
+            initCaseMap(structured.getCaseDate(), structured.getPdate(), structured.getCaseNo(), caseMap);
+        }
+        for(StdLegalEnterpriseExecutedTemp enterpriseExecuted: enterpriseExecutedList) {
+            initCaseMap(enterpriseExecuted.getCaseCreateTime(), null, enterpriseExecuted.getCaseCode(), caseMap);
+        }
+        for(StdLegalEntUnexecutedTemp entUnexecuted : entUnexecutedList) {
+            initCaseMap(entUnexecuted.getRegDate(), entUnexecuted.getPublishDate(), entUnexecuted.getCaseCode(), caseMap);
+        }
+        //获取当前时间
+        LocalDate dateNow = LocalDate.now();
+        int yearNow = dateNow.getYear();
+        int monthNow = dateNow.getMonthValue();
+        if(monthNow > 6){
+            if(caseMap.get(yearNow) != null &&
+                    caseMap.get(yearNow-1) != null && caseMap.get(yearNow-2) != null) {
+                return "频繁";
+            }else{
+                return "else";
+            }
+        }else{
+            if(caseMap.get(yearNow) != null && caseMap.get(yearNow-1) != null
+                    && caseMap.get(yearNow-2) != null && caseMap.get(yearNow-3) != null) {
+                return "频繁";
+            }else{
+                return "else";
+            }
+        }
+    }
+
+    public void initCaseMap(LocalDate caseDate, LocalDate pdate, String caseno, Map<Integer, Integer> caseMap) {
+        Integer caseYear = getCaseDate(caseDate, pdate, caseno);
+        if(caseYear != null) {
+            if(caseMap.containsKey(caseYear)) {
+                caseMap.put(caseYear, caseMap.get(caseYear)+1);
+            }else{
+                caseMap.put(caseYear, 1);
+            }
+        }
+    }
+
+    /**
+     * 获取案件年份
+     * 若【立案时间】为空，则取【发布时间】；若【发布时间】为空或无该字段，则取从【案号】解析出的年份；若【案号】为空，则该案件的【案件年份】视为空。
+     * @return
+     */
+    public Integer getCaseDate(LocalDate caseDate, LocalDate pdate, String caseno) {
+        if(caseDate != null) {
+            return caseDate.getYear();
+        }
+        if(pdate != null) {
+            return pdate.getYear();
+        }
+        if(caseno != null) {
+            Pattern r = Pattern.compile(REG_PATTERN);
+            Matcher m = r.matcher(caseno);
+            if (m.find( )) {
+                String year = m.group(0).trim();
+                return Integer.valueOf(year.substring(1, year.length() -1));
+            } else {
+                return null;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ user_model/src/main/java/com/springboot/mapper/StdLegalDataStructuredTempMapper.java
@@ -0,0 +1,7 @@
+package com.springboot.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.springboot.domain.risk.StdLegalDataStructuredTemp;
+
+public interface StdLegalDataStructuredTempMapper extends BaseMapper<StdLegalDataStructuredTemp> {
+}
