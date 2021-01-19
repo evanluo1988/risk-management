84cdad3f1fde5184b2b5f6a7ac43d401e8e2c24e
lhf <18309225600@163.com>
2021-01-19 18:19:45 +0800

    fix

--- report_model/src/main/java/com/springboot/controller/ReportController.java
+++ report_model/src/main/java/com/springboot/controller/ReportController.java
@@ -1,25 +1,40 @@
 package com.springboot.controller;
 
+import com.springboot.ret.ReturnT;
+import com.springboot.service.ReportService;
+import com.springboot.utils.ReturnTUtils;
+import com.springboot.vo.InformTop10Vo;
+import com.springboot.vo.PendingListVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("report")
+import java.util.List;
+
+@RestController
+@RequestMapping("/report")
 public class ReportController {
+
+    @Autowired
+    private ReportService reportService;
 
     /**
      * 获取举报对象top10的记录
      */
     @GetMapping("/informstop")
-    public void informsTop10() {
-
+    public ReturnT informsTop10() {
+        List<InformTop10Vo> informTop10Vos = reportService.informsTop10();
+        return ReturnTUtils.getReturnT(informTop10Vos);
     }
 
     /**
      * 待办事项
      */
     @GetMapping("/pendinglist")
-    public void pendingList() {
-
+    public ReturnT pendingList() {
+        PendingListVo pendingListVo = reportService.pendingList();
+        return ReturnTUtils.getReturnT(pendingListVo);
     }
 
     /**
